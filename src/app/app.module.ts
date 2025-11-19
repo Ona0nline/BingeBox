4fbf8cfa2b017befd56312659d60ae7236955856
Onalerona Zwane <onzwanejhb024@student.wethinkcode.co.za>
2025-11-19 14:52:34 +0200

    Need to install angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { WatchedComponent } from './watched/watched/watched.component';
 import { ToWatchComponent } from './towatch/to-watch/to-watch.component';
+// import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
