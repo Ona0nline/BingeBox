512ec0418ea2f6f9c6f268a6c872e5ace8e67e0a
Onalerona Zwane <onzwanejhb024@student.wethinkcode.co.za>
2025-11-19 13:46:09 +0200

    Really what could the issue be

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { WatchedComponent } from './watched/watched/watched.component';
+import { ToWatchComponent } from './towatch/to-watch/to-watch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', component:HomeComponent},
+  {path:"/watched", component:WatchedComponent},
+  {path:"/towatch", component:ToWatchComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { WatchedComponent } from './watched/watched/watched.component';
+import { ToWatchComponent } from './towatch/to-watch/to-watch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WatchedComponent,
+    ToWatchComponent
   ],
   imports: [
     BrowserModule,
